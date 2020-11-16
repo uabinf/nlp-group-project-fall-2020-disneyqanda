c26ec1fea42531f02c8fb95938218a0c4d4beefc
Leigh D Allison <leighg@uab.edu>
2020-11-15 22:12:45 -0600

    Trying to get pip install working

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="nlp-group-project-fall-2020-disneyqanda", 
+    name="disneyqanda", 
     version="0.0.1",
     author="Deeptha Srirangam, Leigh Allison",
     author_email="",
