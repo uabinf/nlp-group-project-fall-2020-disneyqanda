787f19b401e09135d9c261ebf02cdfe71d1af0df
Leigh D Allison <leighg@uab.edu>
2020-11-15 21:45:22 -0600

    Add initial setup file

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nlp-group-project-fall-2020-disneyqanda", 
+    version="0.0.1",
+    author="Deeptha Srirangam, Leigh Allison",
+    author_email="deeptha@uab.edu, leighg@uab.edu",
+    description="Disney Q and A",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/uabinf/nlp-group-project-fall-2020-disneyqanda",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
